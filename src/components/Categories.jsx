8fd57e7527074737204422c8b6daaf26e49faf7a
Maksim Chumack <stormspirit100k@gmail.com>
2024-01-19 18:41:30 +0200

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['All', 'Meat', 'Vegeterian', 'Grill', 'Spicy', 'Closed'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
